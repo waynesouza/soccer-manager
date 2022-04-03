d0aea61f97fbd9b9f584e1083e2dff723f952a12
wayne <waynesouza07@gmail.com>
2022-04-03 18:43:14 -0300

    Removendo

--- util/constants.py
+++
@@ -1,6 +0,0 @@
-class Constants:
-    goal_text = 'GOOOOOL !'
-    no_goal_text = 'para fora'
-    pass_text = ['toca para', 'faz um lançmaneto para', 'uma bola enfiada para']
-    shoot_text = ['olha a batidaa!', 'de bicicleta', 'um petardo de fora da área']
-    defend_text = ['ótimo desarme', 'bloqueado']
