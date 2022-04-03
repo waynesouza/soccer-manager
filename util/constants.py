19d1b83aa4551605d90864679c1a7fa5c5ba04dc
wayne <waynesouza07@gmail.com>
2022-04-03 18:30:49 -0300

    Criação das constantes

---
+++ util/constants.py
@@ -0,0 +1,6 @@
+class Constants:
+    goal_text = 'GOOOOOL !'
+    no_goal_text = 'para fora'
+    pass_text = ['toca para', 'faz um lançmaneto para', 'uma bola enfiada para']
+    shoot_text = ['olha a batidaa!', 'de bicicleta', 'um petardo de fora da área']
+    defend_text = ['ótimo desarme', 'bloqueado']
